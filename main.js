d67614e0e87036f1c9c9e9579a7b74ad8b27ff3d
rzhou1999 <randyzhou1999@gmail.com>
2019-05-01 22:14:15 -0400

    Revamped header to new visual terminal-esque style

--- main.js
+++ main.js
@@ -1,23 +1,26 @@
+var typed = undefined;
+
 function showProjects() {
+    typeNormal("cd ~/projects<br>","~/projects");
     document.getElementById("content").innerHTML = multiline(function(){/*
       <h1 class="title">Projects</h1>
 
-      <div class="project-tab project-thiswebsite" onclick="location.href='https://github.com/rzhou1999/rzhou1999.github.io';">
+      <div class="project-tab project-thiswebsite" onclick="window.open('https://github.com/rzhou1999/rzhou1999.github.io');typeNormal('cat web/index.html<br>','~/projects');">
         <h1 class="tab-title">This Website</h1>
         <p class="tab-desc">The very website you're viewing right now.<br><br>Static webpage built from scratch using HTML, CSS and a touch of JavaScript.</p>
       </div>
 
-      <div class="project-tab project-malrecgraph" onclick="location.href='http://randyzhou.com/MALRecGraph/';">
+      <div class="project-tab project-malrecgraph" onclick="window.open('http://randyzhou.com/MALRecGraph/');typeNormal('cat web/MALRecGraph.html<br>','~/projects');">
         <h1 class="tab-title">MALRecGraph</h1>
         <p class="tab-desc">Visualizer for MyAnimeList userrecs.<br><br>Front end written in JavaScript using vis.js for visualization. Back end written in Python with Flask, Connexion and BeautifulSoup.</p>
       </div>
 
-      <div class="project-tab project-stockmarketproject" onclick="location.href='https://github.com/rzhou1999/Stock-Market-Project';">
+      <div class="project-tab project-stockmarketproject" onclick="window.open('https://github.com/rzhou1999/Stock-Market-Project');typeNormal('ls python/Stock_Market_Project<br>','~/projects');">
         <h1 class="tab-title">Stock Market Project</h1>
         <p class="tab-desc-noimg">Program for automatically tracking and identifying stock splits from Yahoo news.<br><br>Developed for my senior project during high school under Max Eringros of G6 Capital. Written in Python with a MySQL database, using YQL to retrieve information and basic machine learning to filter out and identify stock splits.</p>
       </div>
 
-      <div class="project-tab project-stockmarketproject" onclick="location.href='https://github.com/rzhou1999/repapp';">
+      <div class="project-tab project-stockmarketproject" onclick="window.open('https://github.com/rzhou1999/repapp');typeNormal('ls web/repapp<br>','~/projects');">
         <h1 class="tab-title">College Rep Visit App Backend</h1>
         <p class="tab-desc-noimg">System for scheduling college rep visits to the Commonwealth School of Boston.<br><br>Created during my senior year of high school by the Commonwealth School's CS4 class. Written in JavaScript / Node.js with a PostgreSQL database.</p>
       </div>
@@ -25,6 +28,7 @@ function showProjects() {
   }
 
 function showAbout() {
+  typeNormal("cd ~/about<br>","~/about");
   document.getElementById("content").innerHTML = multiline(function(){/*
     <h1 class="title">About</h1>
 
@@ -44,19 +48,23 @@ function showAbout() {
 }
 
 function showLinks() {
+
+  typeNormal("cd ~/links<br>","~/links");
+
+  //document.getElementById("terminal-text").innerHTML += "guest@randyzhou:~$ cd ~/links<br>";
   document.getElementById("content").innerHTML = multiline(function(){/*
     <h1 class="title">Links</h1>
 
     <div class="links">
-      <div class="link" onclick="location.href='resume_web.pdf';">
+      <div class="link" onclick="window.open('resume_web.pdf');typeNormal('cat resume.pdf<br>','~/links');">
         <h3 class="link-title">Resume</h3>
         <span class="mega-octicon octicon-file-pdf"></span>
       </div>
-      <div class="link" onclick="location.href='https://github.com/rzhou1999/';">
+      <div class="link" onclick="window.open('https://github.com/rzhou1999/');typeNormal('ls github<br>','~/links');">
         <h3 class="link-title">GitHub</h3>
         <span class="mega-octicon octicon-mark-github"></span>
       </div>
-      <div class="link" onclick="showEmail()">
+      <div class="link" onclick="showEmail();">
         <h3 class="link-title">Email</h3>
         <span class="mega-octicon octicon-mail"></span>
       </div>
@@ -72,8 +80,41 @@ function showEmail(){
   state = !state;
   if (state){
     document.getElementById("email").innerHTML = "<p class='title'>randyzhou1999 at gmail dot com</p>";
+    typeNormal('cat super-seret-email.txt<br>','~/links');
   }
   else{
     document.getElementById("email").innerHTML = "";
   }
+}
+
+var currDir = "";
+
+function typeNormal(inp, newDir){
+
+  if(!typed.typingComplete){
+    return;
+  }
+
+  var savedText = document.getElementById("terminal-text-saved").innerHTML.split("<br>");
+  console.log(savedText);
+  if (savedText.length >= 7){
+      while (savedText.length >= 7){
+        savedText.shift();
+      }
+      savedText = savedText.join("<br>");
+        console.log(savedText);
+    }
+  else{
+    savedText = document.getElementById("terminal-text-saved").innerHTML;
+  }
+
+  var temp = document.getElementById("terminal-text").innerHTML.split("<br>");
+  var prev = temp.pop();
+  document.getElementById("terminal-text-saved").innerHTML = savedText + temp.join("<br>")+"<br>";
+  var options = {
+    strings: ["`"+prev+"`"+inp +"^250 `guest@randyzhou:"+newDir+"$ `"],
+    typeSpeed: 15
+  };
+  typed.destroy();
+  typed = new Typed("#terminal-text", options);
 }
