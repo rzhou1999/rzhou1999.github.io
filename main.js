4c3c561e318607b9ea28f3f55ae791e198986d04
rzhou1999 <randyzhou1999@gmail.com>
2020-11-20 21:49:57 -0500

    yearly update

--- main.js
+++ main.js
@@ -36,30 +36,26 @@ function showProjects() {
             <p class="tab-desc">The very website you're viewing right now.<br><br>Static webpage built 100% from scratch using HTML, CSS and native JavaScript. Just as it should be.</p>
           </div>
 
+          <div class="project-tab project-iro" onclick="typeNormal('cat web/Iro.html','~/projects',(()=>{window.open('http://randyzhou.com/Iro/');}));">
+            <h1 class="tab-title">Iro</h1>
+            <p class="tab-desc">An interactive chromatic movie visualizer.<br><br>Written for a Cornell class (ASIAN 2252) project using D3.js, Color Thief and copious amounts of duct tape.</p>
+          </div>
+
           <div class="project-tab project-malrecgraph" onclick="typeNormal('cat web/MALRecGraph.html','~/projects',(()=>{window.open('http://randyzhou.com/MALRecGraph/');}));">
             <h1 class="tab-title">MALRecGraph</h1>
             <p class="tab-desc">Visualizer for MyAnimeList userrecs.<br><br>Front end written in JavaScript using vis.js for visualization. Back end written in Python with Flask, Connexion and BeautifulSoup.</p>
           </div>
 
           <div class="project-tab project-logisim" onclick="typeNormal('ls comparch/logisim-evolution','~/projects',(()=>{window.open('https://github.com/rzhou1999/logisim-evolution');}));">
-            <h1 class="tab-title">Feature development for Logisim Evolution</h1>
-            <p class="tab-desc">Development on Logisim Evolution, Cornell CS3410's fork of the digital logic simulator Logisim. Features include a batch attribute change option for Logisim components and a more comprehensive changelog feature using Swing. Written in Java.</p>
+            <h1 class="tab-title">Logisim Evolution</h1>
+            <p class="tab-desc">Feature development on Logisim Evolution, Cornell CS3410's fork of the digital logic simulator Logisim. Features include a batch attribute change option for Logisim components and a more comprehensive changelog. Written in Java.</p>
           </div>
 
           <div class="project-tab project-terminaltyper" onclick="typeNormal('ls web/terminaltyper','~/projects',(()=>{window.open('https://github.com/rzhou1999/terminaltyper');}));">
             <h1 class="tab-title">terminaltyper</h1>
             <p class="tab-desc">A lightweight, fast and easy-to-use typer for simulating terminals in native js, used to power this very page. Written in a single codepen session (so please excuse the mess).</p>
           </div>
-
-          <div class="project-tab project-stockmarketproject" onclick="typeNormal('ls python/Stock_Market_Project','~/projects',(()=>{window.open('https://github.com/rzhou1999/Stock-Market-Project');}));">
-            <h1 class="tab-title">Stock Market Project</h1>
-            <p class="tab-desc-noimg">Program for automatically tracking and identifying stock splits from Yahoo news.<br><br>Developed for my senior project during high school under Max Eringros of G6 Capital. Written in Python with a MySQL database, using YQL to retrieve information and basic machine learning to filter out and identify stock splits.</p>
-          </div>
-
-          <div class="project-tab project-stockmarketproject" onclick="typeNormal('ls web/repapp','~/projects',(()=>{window.open('https://github.com/rzhou1999/repapp');}));">
-            <h1 class="tab-title">College Rep Visit App Backend</h1>
-            <p class="tab-desc-noimg">System for scheduling college rep visits to the Commonwealth School of Boston.<br><br>Created during my senior year of high school by the Commonwealth School's CS4 class. Written in JavaScript / Node.js with a PostgreSQL database.</p>
-          </div>`;
+          `;
     })
   );
 }
@@ -69,14 +65,14 @@ function showAbout() {
     document.getElementById("content").innerHTML =
       `<h1 class="title">About</h1>
 
-        <p>I am a computer scientist studying at Cornell University interested in computer architecture and systems.
+        <p>I am a computer scientist studying at Cornell University. I am interested in the design and utilization of computer architecture and systems for datacenter efficiency and performance.
         </p>
 
         <h2 class="subtitle">Academic</h2>
         <p>
         I graduated from Commonwealth High School of Boston in 2017. I am currently a fourth year university student majoring in CS and minoring in China and Asia Pacific Studies, Asian American Studies and Asian Studies. I plan to apply to graduate programs in the fields of computer architecture and distributed systems this Fall.
         <br><br>
-        I have been conducting research with Cornell's <a href="https://sail.ece.cornell.edu/">SAIL group</a> under Professor Christina Delimitrou since Fall 2019, where I have primarily worked with applications of processing-in-memory in datacenter settings.
+        I have been conducting research with Cornell's <a href="https://sail.ece.cornell.edu/">SAIL group</a> under Professor Christina Delimitrou since Fall 2019, where I have previously worked with applications of processing-in-memory in datacenter settings. I am currently investigating the effects of garbage collection on cloud microservice performance for use in a deployment debugging system based on unsupervised machine learning.
         <br>
         </p>
 
@@ -84,7 +80,7 @@ function showAbout() {
         <p>
         From Spring 2019 to Spring 2020, I was part of the <a href="https://www.cs.cornell.edu/courses/cs3410/2019sp/">Cornell CS 3410 Computer System Organization and Programming</a> course staff as a teaching assistant, where I have been twice nominated by professors for student TA recognition. Throughout my time as a CS3410 TA, I played a crucial part in the CS3410 ISA change from MIPS to RISCV by porting and rewriting major grading scripts for some of the largest/most complex projects in the course, and continue to manage autograder framework and deployment to this day.
         <br><br>
-        As of Fall 2020, I have joined the <a href="https://www.cs.cornell.edu/courses/cs5414/2020fa/">CS 5414 Distributed Computing Principles</a> course staff.
+        As of Fall 2020, I have joined the course staff of <a href="https://www.cs.cornell.edu/courses/cs5414/2020fa/">CS 5414 Distributed Computing Principles</a>, a graduate-level distributed systems class, as a TA.
         <br>
         </p>
 
@@ -150,6 +146,6 @@ function typeNormal(inp, newDir, cb){
 }
 
 function squelchface(){
-  document.getElementById("content").innerHTML = '<center><iframe width="560" height="315" src="https://www.youtube.com/embed/BQStjCPV2EU?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><p>I usually put a song I like here.</p></center>'
+  document.getElementById("content").innerHTML = '<center><iframe width="560" height="315" src="https://www.youtube.com/embed/aCiGnDy9qCo?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><p>I usually put a song I like here.</p></center>'
   typeNormal("yes that is my face ","~",(function(){return;}));
 }
